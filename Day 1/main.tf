feba87181f4cb4067b28a976252fed527c102f7a
krunalijain <krunalijain2001@gmail.com>
2024-03-08 14:24:20 +0000

    EC2 Launched via Terraform script

--- Day 1/main.tf
+++ Day 1/main.tf
@@ -1,3 +1,10 @@
 provider "aws" {
-    
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-07d9b9ddc6cd8dd30"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0baa3d3790f07594b"
+    key_name = "aws-login"
 }
