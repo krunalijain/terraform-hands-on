479d7ae12c6dabf4604a4834459c6f13e9c08543
Krunali <115455157+krunalijain@users.noreply.github.com>
2024-03-09 09:45:05 +0530

    removed hard-coded values and added comments for with description

--- Day 1/main.tf
+++ Day 1/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-1"  # region as per your preference
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-07d9b9ddc6cd8dd30"
+    ami = "ami-07d9b9ddc6cd8dd30" # can take it from AWS > EC2 > Launch Instance > Select any AMI (Ubuntu, RedHat, etc.) > grab the ami ID.
     instance_type = "t2.micro"
-    subnet_id = "subnet-0baa3d3790f07594b"
-    key_name = "aws-login"
+    subnet_id = "your-subnet-id" # Eg: subnet-0aab3d3790f07570c
+    key_name = "aws-login" # create a key-pair (.pem / .ppk).
 }
